6d30e2317966552d6c9c5764cfcd4d5b767a9265
CalebNdumbu <calebkyamaweb@gmail.com>
2024-07-28 23:17:13 +0300

    quick sort

--- code.c
+++ code.c
@@ -2,40 +2,80 @@
 #include "sort.h"
 
 /**
- * selection_sort - Sorts an array of integers in ascending order
- *                  using the Selection sort algorithm
+ * quick_sort - Sorts an array of integers in ascending order
+ *              using the Quick sort algorithm
  * @array: The array to be sorted
  * @size: The size of the array
  */
-
-void selection_sort(int *array, size_t size)
+void quick_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx;
-    int temp; //temp is used for swapping elements
- 
-    //loop through the entire array except the last element
-    for (i = 0; i < size - 1; i++)
+    if (array == NULL || size < 2)
+        return;
+
+    quick_sort_recursive(array, 0, size - 1, size);
+}
+
+/**
+ * quick_sort_recursive - Recursively sorts sub-arrays
+ * @array: The array to be sorted
+ * @low: The starting index of the sub-array
+ * @high: The ending index of the sub-array
+ * @size: The size of the array
+ */
+void quick_sort_recursive(int *array, int low, int high, size_t size)
+{
+    if (low < high)
     {
-        //assume the current element is the minimum
-        min_idx = i;
+        int pivot_index = lomuto_partition(array, low, high, size);
+        quick_sort_recursive(array, low, pivot_index - 1, size);
+        quick_sort_recursive(array, pivot_index + 1, high, size);
+    }
+}
 
-        //find min element in unsorted part of array
-        for(j = i + 1; j < size; j++)
+/**
+ * lomuto_partition - Partitions the array using the Lomuto scheme
+ * @array: The array to be partitioned
+ * @low: The starting index of the partition
+ * @high: The ending index of the partition
+ * @size: The size of the array
+ *
+ * Return: The index of the pivot element
+ */
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+    int pivot = array[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++)
+    {
+        if (array[j] <= pivot)
         {
-            if (array[j] < array[min_idx]) //if a smaller element is found
-                min_idx = j;//update idx of min element
-        }
-
-        //if the min element is not the current element, swap them
-
-        if (min_idx != 1)
-        {
-            temp = array[min_idx];
-            array[min_idx] = array[i];
-            array[i] = temp;
-
-            print_array(array, size);
+            i++;
+            if (i != j)
+            {
+                swap(&array[i], &array[j]);
+                print_array(array, size);
+            }
         }
     }
 
+    if (i + 1 != high)
+    {
+        swap(&array[i + 1], &array[high]);
+        print_array(array, size);
+    }
+
+    return i + 1;
+}
+
+/**
+ * swap - Swaps two integers
+ * @a: The first integer
+ * @b: The second integer
+ */
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
